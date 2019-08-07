e8e34cba70a25976f1ace1238dd134ab24b6b02e
tmiller17 <thomas.miller@metlife.com>
2019-08-07 15:00:44 -0400

    created getCBRT method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -14,27 +14,43 @@ public class Main {
             int sum = 0;
 
             for (int i = 0; i < 9; i++) {
-                int highest = 0;
-                for (int j = 0; j < num; j++) {
 
-                    if (Math.cbrt(num) % j == 0) {
-                        highest = j;
-                    }
-                }
+                double temp = num;
 
-                if (highest == 0) {
-                    highest = 1;
-                }
+                System.out.println((int)getCBRT(num) + " cubed");
 
-                System.out.print(highest + " cubed + ");
-                sum += highest;
-                num = highest;
+                sum += Math.pow(getCBRT(temp), 3); // cubes the cbrt and adds it to the sum
+                temp -= Math.pow(getCBRT(temp), 3); // cubes the cbrt and subtracts it from the sum
 
-            } // end of outer for
+                if (sum == num) { break; } // ends the loop if sum equals original number
+            }
+
             System.out.println("Sum: " + sum);
         } catch (Exception e) {
             System.out.println("Error: Invalid Input");
         }
 
     }
+
+    static double getCBRT(double num) {
+
+        int highest = 0;
+
+        for (int i = 1; i <= 9; i++) {
+
+            for (int j = 0; j < num; j++) {
+                if (Math.pow(j, 3) <= num) {
+                    highest = j;
+                }
+            }
+
+            if (highest == 0) {
+                return 1;
+            } else {
+                return highest;
+            }
+        }
+        return highest;
+    }
+
 }
