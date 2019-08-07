ee67aaf7d78d02656d8afb5991e4bfeff1177d79
tmiller17 <thomas.miller@metlife.com>
2019-08-07 15:13:44 -0400

    added author, cleaned up code, added more comments

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,10 @@ package com.company;
 
 import java.util.Scanner;
 
+                                                /***************
+                                                Author: @tcm5343
+                                                ***************/
+
 public class Main {
 
     public static void main(String[] args) {
@@ -9,43 +13,37 @@ public class Main {
         try {
             Scanner input = new Scanner(System.in);
             System.out.println("Enter a number: ");
-
             double num = input.nextDouble();
             int sum = 0;
-            double temp = num;
+            double remainder = num;
 
             for (int i = 0; i < 9; i++) {
 
-                System.out.println((int)getCBRT(temp) + " cubed");
+                System.out.println((int)getCBRT(remainder) + " cubed");
 
-                sum += Math.pow(getCBRT(temp), 3); // cubes the cbrt and adds it to the sum
-                temp -= Math.pow(getCBRT(temp), 3); // cubes the cbrt and subtracts it from the sum
+                double cube = Math.pow(getCBRT(remainder), 3); // cubes the cbrt
+                sum += cube; // adds the cube to the sum
+                remainder -= cube; // subtracts the cube from the sum
 
-                if (sum == num) { break; } // ends the loop if sum equals original number
+                if (sum == num) { break; } // checks if sum equals original number
             }
-
             System.out.println("Sum: " + sum);
         } catch (Exception e) {
             System.out.println("Error: Invalid Input");
         }
-
     }
 
     private static double getCBRT(double num) {
-
         int highest = 0;
 
+        // loop finds highest square root in the number and returns it
             for (int j = 0; j < num; j++) {
                 if (Math.pow(j, 3) <= num) {
                     highest = j;
                 }
             }
-
-            if (highest == 0) {
-                return 1;
-            } else {
-                return highest;
-            }
+            // checks highest digit
+            if (highest == 0) { return 1; }
+            else { return highest; }
     }
-
 }
