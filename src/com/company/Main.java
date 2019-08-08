ba89c9e7cb1d2c9d1656dd93acf47668519930a9
tmiller17 <thomas.miller@metlife.com>
2019-08-08 09:30:46 -0400

    changed loop to decrement to improve performance

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,9 +13,9 @@ public class Main {
         try {
             Scanner input = new Scanner(System.in);
             System.out.println("Enter a positive integer: ");
-            long num = Math.abs(input.nextLong());
+            int num = Math.abs(input.nextInt());
             int sum = 0;
-            long remainder = num;
+            int remainder = num;
 
             for (int i = 1; i <= 9; i++) { // 9 is the limit of cubes in LE Dickson's proof
 
@@ -36,20 +36,29 @@ public class Main {
         }
     }
 
-    private static long getCBRT(long num) {
+    private static int getCBRT(int num) {
 
         int highest = 0;
 
-        // loop finds highest square root in the number and returns it
-        // this is very inefficient for large numbers but it works for now
-        // todo run test to see if decrementing and doing Math.cbrt() is faster?
-            for (int j = 1; j <= num; j++) {
-                if (Math.pow(j, 3) <= num) {
-                    highest = j;
+        // starts at the num and decrements until it finds a cbrt
+            for (int i = num; i >= 0; i--) {
+                double temp = (double)i;
+                // checks if the cbrt is an integer
+                if (Math.cbrt(temp) % 1 == 0) {
+                    return (int)Math.cbrt(temp); }
+                else { highest = 1; }
             }
-            }
-            // checks highest digit
-            if (highest == 0) { return 1; }
-            else { return highest; }
+            return highest;
     }
 }
+
+
+// this loop increments by one finding the highest square root
+// very inefficient for large numbers but saving the code for reference
+
+/*for (int j = 1; j <= num; j++) {
+        if (Math.pow(j, 3) <= num) {
+        highest = j; } }
+        // checks highest digit
+        if (highest == 0) { return 1; }
+        else { return highest; }*/
