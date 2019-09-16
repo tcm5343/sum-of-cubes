3bac08299d29561b36b74a028f17d34076273877
Tom Miller <tommiller@MacBook-Air.local>
2019-09-15 22:34:11 -0400

    starting fresh to plan out program

--- src/com/company/sumOfCubes.java
+++ src/com/company/sumOfCubes.java
@@ -2,9 +2,9 @@ package com.company;
 
 import java.util.Scanner;
 
-                                                /***************
-                                                 Author: @tcm5343
-                                                 ***************/
+/***************
+ Author: @tcm5343
+ ***************/
 
 public class sumOfCubes {
 
@@ -14,22 +14,8 @@ public class sumOfCubes {
             Scanner input = new Scanner(System.in);
             System.out.println("Enter a positive integer: ");
             int num = Math.abs(input.nextInt());
-            int count = 1;
 
-            Boolean test = function(num, getCBRT(num));
-
-            // while (!function(num, (int)Math.pow(getCBRT(num),3))) { // if the function returns false
-            while (!test) { // while the function returns false
-                int highestCBRT = getCBRT(num);
-                if (highestCBRT - count != 0) {
-                    test = function(num, highestCBRT - count);
-                    count++;
-                }
-                else {
-                    System.out.println("This program has failed to produce the correct result");
-                    System.exit(0);
-                }
-            }
+            // insert loop (call to function) here
 
             System.out.println("Program is finished");
             System.exit(0);
@@ -40,58 +26,9 @@ public class sumOfCubes {
         }
     }
 
-    private static Boolean function(int num, int highestCBRT) {
-        int sum = 0;
-        int remainder = num;
-        int count = 0;
-        double cube;
-        boolean proof = false;
-
-        while (sum != num) {
-
-            if (highestCBRT != getCBRT(num)) {
-                System.out.print(highestCBRT + " cubed (" + (int) Math.pow(highestCBRT, 3) + ") + ");
-
-                cube = Math.pow(highestCBRT, 3); // cubes the cbrt
-                sum += cube; // adds the cube to the sum
-                remainder -= cube; // finds the remainder
-                count+=2; // count must be less than or equal to 9 to prove the theory
-            }
-
-            cube = Math.pow(getCBRT(remainder), 3); // cubes the cbrt
-            System.out.print(getCBRT(remainder) + " cubed (" + (int)Math.pow(getCBRT(remainder),3) + ")");
-
-            sum += cube; // adds the cube to the sum
-            remainder -= cube; // finds the remainder
-            count++; // count must be less than or equal to 9 to prove the theory
-
-            if (sum == num || count >= 9) {
-                break;
-            } // checks if sum equals original number
-            else {
-                System.out.print(" + ");
-            } // checks to see if a + is needed
-
-        }
-
-        if (count <= 9 && sum == num) {
-            proof = true;
-        }
-
-        System.out.println();
-        System.out.println("Sum of cubes: " + sum);
-        if (proof) {
-            System.out.println("This proves LE Dickson's Theory");
-            System.out.println("");
-            return true;
-        } else {
-            System.out.println("This does not prove LE Dickson's Theory");
-            System.out.println("");
-            return false;
-        }
-    }
-
-    private static int getCBRT(int num) {
+    // returns the highest cube root (first cube root it finds is divisible)
+// starting from the input and decrementing
+    private static int getDecrementingCBRT(int num) {
         // starts at the num and decrements until it finds a cbrt
         for (int i = num; i >= 0; i--) {
             double temp = (double) i;
@@ -102,15 +39,21 @@ public class sumOfCubes {
         } // end of for loop
         return 1;
     }
-}
 
-
-// this loop increments by one finding the highest square root
-// very inefficient for large numbers but saving the code for reference
-
-/*for (int j = 1; j <= num; j++) {
-        if (Math.pow(j, 3) <= num) {
-        highest = j; } }
+    // returns the highest cube root starting from 1 to the number
+// very slow
+    private static int getIncrementingCBRT(int num) {
+        int highest = 0;
+        for (int j = 1; j <= num; j++) {
+            if (Math.pow(j, 3) <= num) {
+                highest = j;
+            }
+        }
         // checks highest digit
-        if (highest == 0) { return 1; }
-        else { return highest; }*/
+        if (highest == 0) {
+            return 1;
+        } else {
+            return highest;
+        }
+    }
+}
