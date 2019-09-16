34d83b4f04462bb7355ba7d5f3b99575cf50344f
Tom Miller <tommiller@MacBook-Air.local>
2019-09-16 15:00:06 -0400

    created helper methods

--- src/com/company/sumOfCubes.java
+++ src/com/company/sumOfCubes.java
@@ -1,5 +1,7 @@
 package com.company;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Scanner;
 
 /***************
@@ -13,9 +15,12 @@ public class sumOfCubes {
         try {
             Scanner input = new Scanner(System.in);
             System.out.println("Enter a positive integer: ");
-            int num = Math.abs(input.nextInt());
 
-            // insert loop (call to function) here
+            int num = Math.abs(input.nextInt());
+            int[] allCubeRoots = findCBRTs(num); // assigns cube roots to a list
+            int[] cubes = new int[9];
+
+
 
             System.out.println("Program is finished");
             System.exit(0);
@@ -31,29 +36,35 @@ public class sumOfCubes {
     private static int getDecrementingCBRT(int num) {
         // starts at the num and decrements until it finds a cbrt
         for (int i = num; i >= 0; i--) {
-            double temp = (double) i;
             // checks if the cbrt is an integer
-            if (Math.cbrt(temp) % 1 == 0) {
-                return (int) Math.cbrt(temp);
+            if (Math.cbrt((double) i) % 1 == 0) {
+                return (int) Math.cbrt(i);
             }
         } // end of for loop
         return 1;
     }
 
-    // returns the highest cube root starting from 1 to the number
-// very slow
-    private static int getIncrementingCBRT(int num) {
-        int highest = 0;
+    // returns an array which all whole CBRTs fit into
+    private static int[] findCBRTs(int num) {
+        int CBRTcount = getCBRTcount(num); // returns the number of whole cube roots within the number
+        int[] array = new int[CBRTcount];
+        for (int i = 0; i <= array.length-1; i++) { // fills array with incrementing numbers
+            array[i] = i+1;
+            }
+        return array;
+    }
+
+    // returns how many cube roots are in a number
+    private static int getCBRTcount(int num) {
+        int count = 0;
         for (int j = 1; j <= num; j++) {
             if (Math.pow(j, 3) <= num) {
-                highest = j;
+                count++;
             }
         }
         // checks highest digit
-        if (highest == 0) {
-            return 1;
-        } else {
-            return highest;
-        }
+        if (count == 0) { return 1; }
+        else { return count; }
     }
+
 }
